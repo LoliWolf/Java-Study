3b5ab8af60217f03385b10b4847a39f4c892fd7a
LoliWolf <2233284845@qq.com>
2021-12-11 20:43:00 +0800

    v0.21.12.11

---
+++ classTest.java
@@ -0,0 +1,15 @@
+class a{
+    public static void main(String[] args) {
+        System.out.println(3);
+    }
+}
+class b{
+    public static void main(String[] args) {
+        System.out.println("2");
+    }
+}
+public class classTest {
+    public static void main(String[] args) {
+        System.out.println("1");
+    }
+}
