b4b76f9cd439690fb8c158efe0cb7be48ad1a778
LoliWolf <2233284845@qq.com>
2021-12-19 00:27:25 +0800

    v0.21.12.19.00

---
+++ typeChangeDemo.java
@@ -0,0 +1,7 @@
+public class typeChangeDemo {
+    public static void main(String[] args) {
+        long a=2147483647L;
+        System.out.println((int)a);
+        System.out.println(Integer.MAX_VALUE);
+    }
+}
